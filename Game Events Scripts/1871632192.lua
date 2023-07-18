768072543f4dd1198797704a5a4c25e3f2040218
Unknown <109893857+Pygames2Game1Dev@users.noreply.github.com>
2023-07-18 20:17:13 +0800

    Add files via upload

---
+++ Game Events Scripts/1871632192.lua
@@ -0,0 +1,7 @@
+-- Roblox Creator Challenge
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson1')
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson2')
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson3')
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson4')
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson5')
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson6')
---
+++ Game Events Scripts/2546610365.lua
@@ -0,0 +1,4 @@
+-- Creator Challenge Quiz
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson1')
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson2')
+game.ReplicatedStorage.Events.FinishQuiz:InvokeServer('Lesson3')
